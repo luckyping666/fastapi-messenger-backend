370dbe9165c3f55b73b34d1f36f4ae6c50fd4982
Roman <kuklin.prog@gmail.com>
2025-09-23 22:47:54 +0800

    Fix bug in dependencies

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -41,7 +41,7 @@ def get_current_user(
     )
 
     try:
-        payload = jwt.decode(token, settings.JWT_SECRET_KEY, algorithms=[settings.JWT_ALGORITHM])
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
         user_id: int = int(payload.get("sub"))
         if user_id is None:
             raise credentials_exception
