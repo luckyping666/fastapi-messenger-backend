fe75d27b2ffaf8597dbd83d12ecb7bc23b51fc1b
Roman <kuklin.prog@gmail.com>
2025-09-23 23:18:23 +0800

    fix chat_schema

--- app/schemas/chat_schema.py
+++ app/schemas/chat_schema.py
@@ -2,9 +2,15 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import Optional
 
+
+class ChatCreateRequest(BaseModel):
+    user2_id: int  
+
+
 class ChatResponse(BaseModel):
     id: int
     user1_id: int
     user2_id: int
     created_at: datetime
     last_message: Optional[str] = None
+
